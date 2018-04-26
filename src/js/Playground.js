c18372d05eb3294e446e7444a654831cc85ecc12
Datross <mael.crespinpommier@datross.com>
2018-04-27 00:20:24 +0200

    la selection au click devrait marcher, il reste un petit problème faut checker si ça ajoute et enlève bien des objets

--- src/js/Playground.js
+++ src/js/Playground.js
@@ -5,12 +5,17 @@ var PLAYGROUND = {
 
     // Any object visible in the scene (mesh, camera, light...)
     SceneObject: function(geometry, solidColor, wireColor) {
-        if(geometry === undefined) throw "'geometry' not provided.";
+        if (geometry === undefined) throw "'geometry' not provided.";
 
-        this.solidMaterial = new THREE.MeshBasicMaterial({ color: solidColor ? solidColor : 0x009999 });
-        this.wireMaterial = new THREE.LineBasicMaterial({ color: wireColor ? wireColor : 0x000000, linewidth: 1 });
+        this.solidMaterial = new THREE.MeshBasicMaterial({
+            color: solidColor ? solidColor : 0x009999
+        });
+        this.wireMaterial = new THREE.LineBasicMaterial({
+            color: wireColor ? wireColor : 0x000000,
+            linewidth: 1
+        });
         this.mesh = new THREE.Mesh(geometry, this.solidMaterial);
-        this.wire = new THREE.LineSegments(new THREE.EdgesGeometry( geometry ), this.wireMaterial);
+        this.wire = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), this.wireMaterial);
 
         // parent of every other THREE objects.
         this.root = new THREE.Object3D();
@@ -19,10 +24,6 @@ var PLAYGROUND = {
     },
 
     Scene: function() {
-        // dimensions in pixels of the scene viewport
-        this.width = null;
-        this.height = null;
-
         this.generalProperties = {
             // TODO these properties probably need setters to update correctly the scene
             backgroundColor: 0x909090
@@ -30,21 +31,6 @@ var PLAYGROUND = {
 
         // THREE JS Scene
         this.scene = new THREE.Scene();
-
-        // THREE JS Renderer
-        this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setClearColor( this.generalProperties.backgroundColor );
-
-        // THREE JS Camera
-        // TODO check the near/far, maybe relative to the scene dimension?
-        this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000);
-        this.camera.position.set(0, 0, 20);
-        this.camera.lookAt(0, 0, 0);
-        this.camera.position.z = 5;
-
-        // Only one Orbit Control of the scene.
-        this.orbitControls = new THREE.OrbitControls(this.camera);
-        this.orbitControls.update(); // TODO do we need this?
 
         // Selection
         this.selection = {
@@ -60,10 +46,10 @@ var PLAYGROUND = {
 
             add: function(object) {
                 // check if it's already in selection
-                if(this.objects.indexOf(object) == -1) {
+                if (this.objects.indexOf(object) == -1) {
                     this.objects.push(object);
                     this.group.add(object.root);
-                    THREE.attach(object.root, this.scene, this.group);
+                    THREE.SceneUtils.attach(object.root, this.scene, this.group);
                 }
                 // update everything
                 this.update();
@@ -72,9 +58,9 @@ var PLAYGROUND = {
             remove: function(object) {
                 // see if it's in the selection
                 let index = this.objects.indexOf(object);
-                if(index != -1) {
+                if (index != -1) {
                     this.objects.splice(index);
-                    THREE.detach(object.root, this.group, this.scene);
+                    THREE.SceneUtils.detach(object.root, this.group, this.scene);
                 }
                 // update everything
                 this.update();
@@ -82,7 +68,7 @@ var PLAYGROUND = {
 
             clear: function() {
                 let n = this.objects.length;
-                for(let i = 0; i < n; i++) {
+                for (let i = 0; i < n; i++) {
                     this.remove(this.objects[0]);
                 }
             },
@@ -94,24 +80,112 @@ var PLAYGROUND = {
             }
         };
 
+        // 3d grid
+        this.gridHelper = new THREE.GridHelper(5, 10);
+        this.scene.add(this.gridHelper);
+
         // add selection 'group' object to the THREE JS scene
         this.scene.add(this.selection.group);
 
         // update selection to init visibility of transform widget
         this.selection.update();
 
-        // Selection transform widget
-        this.selectionTransformWidget = new THREE.TransformControls( this.camera, this.renderer.domElement );
-        this.selectionTransformWidget.attach(this.selection.group);
-        this.scene.add(this.selectionTransformWidget);
+        // Objects contained in the scene (raytracing scene)
+        this.objects = [];
+
+        // Raycaster to know on which object the user click
+        this.raycaster = new THREE.Raycaster();
+
+        ///////////////////////
+        //// ** METHODS ** ////
+        ///////////////////////
+
+
+        // animate function
+        this.update = function() {
+            this.selection.update();
+        };
+
+        // add an object to the scene
+        this.add = function(object) {
+            this.objects.push(object);
+            this.scene.add(object.root);
+        };
+
+        // remove object from the scene
+        this.remove = function(object) {
+            // remove it from selection if it's selected
+            this.selection.remove(object);
+            // remove from THREE JS scene
+            this.remove(object.root);
+            // remove from objects if it's in
+            let index = this.objects.indexOf(object);
+            if (index != -1) {
+                this.objects.splice(index);
+            }
+        };
+
+        // remove every objects in the scene
+        this.clear = function() {
+            this.selection.clear();
+            this.objects = [];
+        };
+
+        this.raycastSceneObject = function(origin, direction) {
+            this.raycaster.set(origin, direction);
+            // get all the intersection with THREE JS objects
+            let intersects = this.raycaster.intersectObjects(this.scene.children, true);
+            // keep the closest intersection which corresponds to SceneObject.
+            // intersection are sorted by distance so we only need a simple loop and take the first SceneObject
+            for (let i = 0; i < intersects.length; i++) {
+                let object = intersects[i].object;
+                // search for the object in the SceneObject of the scene
+                let sceneObject = this.objects.find(function(element) {
+                    return element.root == object;
+                });
+                // if it's a SceneObject, this the one we are looking for
+                if (sceneObject !== undefined) {
+                    return sceneObject;
+                }
+            }
+            // here no object has been found, so we just return no object
+            return undefined;
+        }
+    },
+
+    Viewport: function(scene) {
+        // Playground scene
+        this.scene = scene;
+
+        // dimensions in pixels of the scene viewport
+        this.width = null;
+        this.height = null;
+
+        // THREE JS Renderer
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setClearColor(this.scene.generalProperties.backgroundColor);
+
+        // THREE JS Camera
+        // TODO check the near/far, maybe relative to the scene dimension?
+        this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000);
+        this.camera.position.set(0, 0, 20);
+        this.camera.lookAt(0, 0, 0);
+        this.camera.position.z = 5;
+
+        // Raycaster to computes selection rays to send to the scene
+        this.raycaster = new THREE.Raycaster();
+
+        // Only one Orbit Control of the scene.
+        this.orbitControls = new THREE.OrbitControls(this.camera);
+        this.orbitControls.update(); // TODO do we need this?
 
         // post processing for outline selected objects
         this.composer = new THREE.EffectComposer(this.renderer);
-	    this.renderPass = new THREE.RenderPass(this.scene, this.camera);
-        this.outlinePass = new THREE.OutlinePass(new THREE.Vector2(this.width, this.height), this.scene, this.camera);
+        this.renderPass = new THREE.RenderPass(this.scene.scene, this.camera);
+        this.outlinePass = new THREE.OutlinePass(new THREE.Vector2(this.width, this.height), this.scene.scene, this.camera);
         let copyPass = new THREE.ShaderPass(THREE.CopyShader);
-	    this.composer.addPass(this.renderPass);
-	    this.composer.addPass(this.outlinePass);
+        this.composer.addPass(this.renderPass);
+        this.composer.addPass(this.outlinePass);
         this.composer.addPass(copyPass);
         // configure passes
         this.outlinePass.visibleEdgeColor.set(0xff0000);
@@ -120,15 +194,12 @@ var PLAYGROUND = {
         this.outlinePass.edgeStrength = 10;
         copyPass.renderToScreen = true;
 
-        // 3d grid
-        this.gridHelper = new THREE.GridHelper(5, 10);
-        this.scene.add(this.gridHelper);
+        // Selection transform widget
+        // TODO move widget in viewport
+        this.selectionTransformWidget = new THREE.TransformControls(this.camera, this.renderer.domElement);
+        this.selectionTransformWidget.attach(this.scene.selection.group);
+        this.scene.scene.add(this.selectionTransformWidget);
 
-        // Objects contained in the scene
-        this.objects = [];
-
-        // Raycaster to know on which object the user click
-        this.raycaster = new THREE.Raycaster();
 
         ///////////////////////
         //// ** METHODS ** ////
@@ -154,83 +225,58 @@ var PLAYGROUND = {
 
         // update outline pass relatively to selected objects
         this.updateOutlinePass = function() {
-            this.outlinePass.selectedObjects = [this.selection.group.children];
+            this.outlinePass.selectedObjects = this.scene.selection.group.children;
         };
 
-        // animate fuction
         this.animate = function() {
             requestAnimationFrame(this.animate.bind(this));
 
             this.orbitControls.update();
             this.selectionTransformWidget.update();
-
+            this.scene.update();
+            this.updateOutlinePass();
             this.composer.render();
         };
 
-        // add an object to the scene
-        this.add = function(object) {
-            this.objects.push(object);
-            this.scene.add(object.root);
-        };
-
-        // remove object from the scene
-        this.remove = function(object) {
-            // remove it from selection if it's selected
-            this.selection.remove(object);
-            // remove from THREE JS scene
-            this.remove(object.root);
-            // remove from objects if it's in
-            let index = this.objects.indexOf(object);
-            if(index != -1) {
-                this.objects.splice(index);
-            }
-        };
-
-        // remove every objects in the scene
-        this.clear = function() {
-            this.selection.clear();
-            this.objects = [];
-        };
-
         // handles user click and returns either null or the selected object
-        this.getClickedObject = function(mouse) {
+        this.selectSceneObject = function(clickEvent) {
+            let mouse = new THREE.Vector2(-1 + 2 * clickEvent.clientX / this.width,
+                                      -1 + 2 * clickEvent.clientY / this.height);
             this.raycaster.setFromCamera(mouse, this.camera);
-            // get all the intersection with THREE JS objects
-            let intersects = this.raycaster.intersectObjects(this.scene.children, true);
-            // keep the closest intersection which corresponds to SceneObject.
-            // intersection are sorted by distance so we only need a simple loop and take the first SceneObject
-            for(let i = 0; i < intersects.length; i++) {
-                let object = intersects[i].object;
-                // search for the object in the SceneObject of the scene
-                let sceneObject = this.objects.find(function(element) {
-                    return element.root == object;
-                });
-                // if it's a SceneObject, this the one we are looking for
-                if(sceneObject !== undefined) {
-                    return sceneObject;
-                }
+            let sceneObject = this.scene.raycastSceneObject(this.raycaster.ray.origin,
+                                                            this.raycaster.ray.direction);
+            // is there is a returned sceneObject
+            if(sceneObject !== undefined) {
+                // TODO modify this
+                this.scene.selection.clear();
+                this.scene.selection.add(sceneObject);
             }
-            // here no object has been found, so we just return no object
-            return undefined;
+        };
+
+        this.callbackClick = function(clickEvent) {
+            this.selectSceneObject(clickEvent);
         };
     }
 };
 
 window.onload = function() {
     var playgroundScene = new PLAYGROUND.Scene();
+    var playgroundViewport = new PLAYGROUND.Viewport(playgroundScene);
 
-    let resizePlaygroundScene = function() {
-        playgroundScene.resize(window.innerWidth, window.innerHeight);
+    let resizePlaygroundViewport = function() {
+        playgroundViewport.resize(window.innerWidth, window.innerHeight);
     };
 
-    resizePlaygroundScene();
-    playgroundScene.attachToDOM(document.body);
-    playgroundScene.animate();
+    resizePlaygroundViewport();
+    playgroundViewport.attachToDOM(document.body);
+    playgroundViewport.animate();
 
-    window.addEventListener( 'resize', resizePlaygroundScene, false );
+    window.addEventListener('resize', resizePlaygroundViewport, false);
+    window.addEventListener('click', playgroundViewport.callbackClick.bind(playgroundViewport), false);
 
 
     // fill scene with a cube
     let cube = new PLAYGROUND.SceneObject(new THREE.BoxGeometry(1, 1, 1));
     playgroundScene.add(cube);
+    playgroundScene.selection.add(cube);
 };
